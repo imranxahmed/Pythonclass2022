daaee617c54de3b9eeda3ff56439151dbf880678
imranxahmed <87036525+imranxahmed@users.noreply.github.com>
2022-10-02 19:17:48 -0400

    classwork 10/2/2022

---
+++ if-then-else.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  2 07:41:40 2022
+
+@author: Imran Ahmed
+"""
+
+''' 
+write a function
+it will remind us of missed dates
+and upcoming dates 
+'''  
+     
+ask_input = input('give me your name: ')
+
+if ask_input == 'lost':
+    
+    '''run below true statemetts'''
+    print('Hey you are: \t' , ask_input)
+
+elif ask_input == 'drama':
+    
+    print('Please stop the: \t' , ask_input)
+    
+elif ask_input == 'memory':
+    
+    print('Wow you have no: \t' , ask_input)
+    
+else:
+    print('\n Dude u wasted my time ...')
+    
---
+++ make_a_list.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+def my_func():
+    
+    get_me_something = 'apples'
+    eat_only = 2
+    kill_the_cat = 'yes'
+    print('Get me some: ', get_me_something , 'I only have {} meals' , eat_only, 
+          'You would kill the cat:', kill_the_cat )
+
+ask_Name =  input('Name: ')
+
+ask_Age = input('Age:')
+
+ask_Hobby = input('Hobby: ')
+
+ask_Serious = input('Ar u serious: ')
+
+print( "welcome: " , ask_Name ,   '\n' , "U r too old: " , ask_Age ,
+       '\n', "your hobby: " , ask_Hobby , '\n', "serious: " , ask_Serious)
+
+''' calling my function '''
+my_func()
+
+
