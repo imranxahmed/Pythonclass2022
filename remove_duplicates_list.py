676d3dd73cbe5bc73f90240cff5fc4a00959db80
imranxahmed <87036525+imranxahmed@users.noreply.github.com>
2023-01-29 12:37:09 -0500

    remove duplicates from a list

--- remove_duplicates_list.py
+++ remove_duplicates_list.py
@@ -16,28 +16,29 @@ print('Main list: ', same, end='\n\n')
 while count < len(same):
     if same[count].__eq__(match):
         print('index={0} --> Match poped:{1}'.format(count, same.pop(count)))
-        count -=1
+
         print('New index: {0}  --> New len: {1}'.format(count, len(same)))
     else:
         print('skipped index:', count)
         print('List length: {0} \tList: {1}'.format(len(same),same))
         count +=1
         
-print('End of duplicate removel. \nFinal list: {0}'.format(same))
+print('End of duplicate removel. \n\nFinal list: {0}'.format(same))
 
 
-# =============================================================================
 # dups = ['next', 'come', 'next', 'come', 'next', 'next', 'built', 'come', 'also']
-# 
+# print('\nMain list: ', dups, end='\n\n')
+
 # for index, item in enumerate(dups):
+#     print('checking index: {0}'.format(index))
 #     if item.__eq__(match):
 #         print('poped at Index: {0}'.format(index))
 #         dups.pop(index)
 #         print('New Length: {0} \tNew List: {1}'.format(len(dups), dups))
+#         print('Index is: {0}'.format(index), end='\n\n')
 #     else:
 #         print('skiped Index: {0}'.format(index))
-#         
-#         
+        
+        
 # print('All duplicates removed. \n Final List: ', dups)
-# 
-# =============================================================================
+
